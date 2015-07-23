5e341be85d3a2240ebf3bcd4705c1f556f080ed7
fewstera <aidan.fewster@bbc.co.uk>
2015-07-23 14:51:53 +0100

    Added lightbox

--- source/js/lightboxHandler.js
+++ source/js/lightboxHandler.js
@@ -1,10 +1,33 @@
 define(['lib/news_special/bootstrap'], function (news) {
 
-    news.pubsub.on('lightbox:open', function openLightbox(openObject) {
+    var lightboxEl = news.$('.lightbox--overlay'),
+        closeButtonEl = lightboxEl.find('.lightbox--close-button'),
+        titleEl = lightboxEl.find('.lightbox--title'),
+        messageEl = lightboxEl.find('.lightbox--message');
+
+    function openLightbox(openObject) {
         var title = openObject.title,
             messageMarkup = openObject.messageMarkup;
 
-        console.log(title, messageMarkup);
-    });
+        titleEl.text(title);
+        messageEl.html(messageMarkup);
+        lightboxEl.show();
 
+    }
+
+    function closeLightbox(e) {
+        if (e && e.target !== this) {
+            return;
+        }
+
+        lightboxEl.hide();
+    }
+
+
+    /* EVENT LISTENERS */
+    news.pubsub.on('lightbox:open', openLightbox);
+
+    news.pubsub.on('lightbox:close', closeLightbox);
+    lightboxEl.on('click', closeLightbox);
+    closeButtonEl.on('click', closeLightbox);
 });
